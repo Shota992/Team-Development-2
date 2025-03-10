99d90fde07b35184db6416acd6b519ec09e21c29
Sakura396 <sakusaku00707@gmal.com>
2025-03-10 16:04:43 +0900

    nullableは後で話し合う。ログインの遷移まで成功

--- src/database/migrations/2025_03_10_053440_add_status_columns_to_users_table.php
+++ src/database/migrations/2025_03_10_053440_add_status_columns_to_users_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->date('birthday');
-            $table->tinyInteger('gender');
-            $table->unsignedBigInteger('office_id');
+            $table->date('birthday')->nullable();
+            $table->tinyInteger('gender')->nullable();
+            $table->unsignedBigInteger('office_id')->nullable();
             $table->foreign('office_id')->references('id')->on('offices');
-            $table->unsignedBigInteger('department_id');
+            $table->unsignedBigInteger('department_id')->nullable();
             $table->foreign('department_id')->references('id')->on('departments');
-            $table->unsignedBigInteger('position_id');
+            $table->unsignedBigInteger('position_id')->nullable();
             $table->foreign('position_id')->references('id')->on('positions');
-            $table->tinyInteger('administrator');
+            $table->tinyInteger('administrator')->nullable();
         });
     }
 
@@ -31,16 +30,14 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('birthday');
-            $table
-                ->dropForeign(['office_id'])
-                ->dropColumn('office_id');
-            $table
-                ->dropForeign(['department_id'])
-                ->dropColumn('department_id');
-            $table
-                ->dropForeign(['position_id'])
-                ->dropColumn('position_id');
+            $table->dropForeign(['office_id']);
+            $table->dropColumn('office_id');
+            $table->dropForeign(['department_id']);
+            $table->dropColumn('department_id');
+            $table->dropForeign(['position_id']);
+            $table->dropColumn('position_id');
             $table->dropColumn('administrator');
         });
     }
 };
+
