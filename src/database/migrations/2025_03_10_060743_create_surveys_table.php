2fab69d57db53d7d5892c1d2228ccc4b577bbaf2
hiroo-road <hirofumi7678@gmail.com>
2025-03-10 15:53:21 +0900

    add:アンケート関連・施策関連のテーブル追加を実行

---
+++ src/database/migrations/2025_03_10_060743_create_surveys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description', 1000);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedBigInteger('office_id');
+            $table->foreign('office_id')->references('id')->on('offices');
+            $table->unsignedBigInteger('department_id');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surveys');
+    }
+};
---
+++ src/database/migrations/2025_03_10_064627_create_evaluations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('measure_id');
+            $table->foreign('measure_id')->references('id')->on('measures');
+            $table->string('keep' , 1000);
+            $table->string('problem' , 1000);
+            $table->string('try' , 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evaluations');
+    }
+};
