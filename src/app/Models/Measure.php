ef1071d61aff6c39e7d1044a64bd49bcf6164506
Sakura396 <sakusaku00707@gmal.com>
2025-03-19 17:01:19 +0900

    ブランチ忘れ

---
+++ src/app/Models/Measure.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Measure extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'office_id', 'department_id', 'title', 'description', 'status', 'evaluation_interval', 'evaluation_status'
+    ];
+
+    // MeasureとTaskのリレーション (1対多)
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
