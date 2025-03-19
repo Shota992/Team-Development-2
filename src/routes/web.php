9d4d6aebaaf612e40d6676d12f9cffe9e34bab36
hiroo-road <hirofumi7678@gmail.com>
2025-03-19 11:40:03 +0900

    add:部署比較ページ欄の作成（途中）

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DepartmentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,7 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/departments', [DepartmentsController::class, 'index'])->name('dashboard');
 });
 
 Route::middleware('auth')->group(function () {
