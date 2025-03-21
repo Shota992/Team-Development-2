8798db1ad2ca469585dd8facd3739efc2e3e3a22
rikako0927 <lihuazisongben@gmail.com>
2025-03-21 15:31:24 +0900

    サイドバー画像追加済み

--- src/resources/views/components/sidebar.blade.php
+++ src/resources/views/components/sidebar.blade.php
@@ -15,62 +15,62 @@
             <div class="text-gray-800 font-semibold text-center px-2 pb-2">オリエンタルランド株式会社</div>
             <div class="text-gray-600 text-sm text-center px-2 pb-4">経営戦略本部</div>
             <div class="flex items-center justify-center gap-8">
-                <img src="./assets/img/ベルicon.png" alt="Bell" class="w-6 h-6">
-                <img src="./assets/img/ログアウトicon.png" alt="Logout" class="w-6 h-6">
+                <img src="images/bellicon.png" alt="Bell" class="w-6 h-6">
+                <img src="images/logouticon.png" alt="Logout" class="w-6 h-6">
             </div>
         </div>
         <nav>
             <ul>
-                {{-- <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
                     <a href="#" class="flex items-center space-x-2 text-gray-700">
-                        <img src="./assets/img/ダッシュボードicon.png" class="w-6 h-6" alt="">
+                        <img src="images/dashboardicon.png" class="w-6 h-6" alt="">
                         <span>ダッシュボード</span>
                     </a>
-                </li> --}}
-                <li @class([
+                </li>
+                {{-- <li @class([
                     'p-4 rounded hover:bg-[#e6f8fe]',
                     'bg-[#e6f8fe]' => request()->routeIs('dashboard')
                 ])>
                     <a href="{{ route('dashboard') }}" class="flex items-center space-x-2 text-gray-700">
-                        <img src="./assets/img/ダッシュボードicon.png" class="w-6 h-6" alt="">
+                        <img src="/src/public/images/dashboardicon.png" class="w-6 h-6" alt="">
                         <span>ダッシュボード</span>
                     </a>
-                </li>
-                {{-- <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                </li> --}}
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
                     <a href="#" class="flex items-center space-x-2 text-gray-700">
-                        <img src="./assets/img/項目別詳細icon.png" class="w-6 h-6" alt="">
+                        <img src="images/item-detail icon.png" class="w-6 h-6" alt="">
                         <span>項目別詳細</span>
                     </a>
-                </li> --}}
-                <li @class([
+                </li>
+                {{-- <li @class([
                     'p-4 rounded hover:bg-[#e6f8fe]',
                     'bg-[#e6f8fe]' => request()->routeIs('detail')
                 ])>
                     <a href="{{ route('detail') }}" class="flex items-center space-x-2 text-gray-700">
-                        <img src="./assets/img/項目別詳細icon.png" class="w-6 h-6" alt="">
+                        <img src="/src/public/images/item-detail icon.png" class="w-6 h-6" alt="">
                         <span>項目別詳細</span>
                     </a>
-                </li>
-                {{-- <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                </li> --}}
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
                     <a href="#" class="flex items-center space-x-2 text-gray-700">
-                        <img src="./assets/img/部署別比較icon.png" class="w-6 h-6" alt="">
+                        <img src="images/dept-compareicon.png" class="w-6 h-6" alt="">
                         <span>部署別比較</span>
                     </a>
-                </li> --}}
-                <li @class([
+                </li>
+                {{-- <li @class([
                     'p-4 rounded hover:bg-[#e6f8fe]',
                     'bg-[#e6f8fe]' => request()->routeIs('compare')
                 ])>
                     <a href="{{ route('compare') }}" class="flex items-center space-x-2 text-gray-700">
-                        <img src="./assets/img/部署別比較icon.png" class="w-6 h-6" alt="">
+                        <img src="/src/public/images/dept-compareicon.png" class="w-6 h-6" alt="">
                         <span>部署別比較</span>
                     </a>
-                </li>
-                {{-- <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                </li> --}}
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
                     <details class="group cursor-pointer">
                         <summary class="flex justify-between items-center text-gray-700">
                             <div class="flex items-center space-x-2">
-                                <img src="./assets/img/施策立案icon.png" class="w-6 h-6" alt="">
+                                <img src="images/policy-createicon.png" class="w-6 h-6" alt="">
                                 <span>施策立案</span>
                             </div>
                             <span class="transition-transform group-open:rotate-90">▶</span>
@@ -81,14 +81,14 @@
                             <li><a href="#" class="text-gray-600 hover:text-blue-600">施策深掘り</a></li>
                         </ul>
                     </details>
-                </li> --}}
-                <li @class([
+                </li>
+                {{-- <li @class([
                     'p-4 rounded hover:bg-[#e6f8fe]',
                     'bg-[#e6f8fe]' => request()->routeIs('measure.*')
                 ])>
                     <details class="group cursor-pointer" @if(request()->routeIs('measure.*')) open @endif>
                         <summary class="flex items-center space-x-2 text-gray-700">
-                            <img src="./assets/img/施策立案icon.png" class="w-6 h-6" alt="">
+                            <img src="/src/public/images/policy-createicon.png" class="w-6 h-6" alt="">
                             <span class="flex-1">施策立案</span>
                             <span class="ml-auto transition-transform group-open:rotate-90">▶</span>
                         </summary>
@@ -98,12 +98,45 @@
                             <li><a href="{{ route('measure.deepdive') }}" class="text-gray-600 hover:text-blue-600">施策深掘り</a></li>
                         </ul>
                     </details>
-                </li>
-                {{-- <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                </li> --}}
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
                     <details class="group cursor-pointer">
                         <summary class="flex justify-between items-center text-gray-700">
                             <div class="flex items-center space-x-2">
-                                <img src="./assets/img/アンケート設定icon.png" class="w-6 h-6" alt="">
+                                <img src="images/policy-listicon.png" class="w-6 h-6" alt="">
+                                <span>施策一覧</span>
+                            </div>
+                            <span class="transition-transform group-open:rotate-90">▶</span>
+                        </summary>
+                        <ul class="pl-6 mt-4 space-y-1">
+                            <li><a href="#" class="text-gray-600 hover:text-blue-600">実行タスク一覧</a></li>
+                            <li><a href="#" class="text-gray-600 hover:text-blue-600">評価/改善未対応タスク一覧</a></li>
+                            <li><a href="#" class="text-gray-600 hover:text-blue-600">評価/改善済みタスク一覧</a></li>
+                        </ul>
+                    </details>
+                {{-- <li @class([
+                    'p-4 rounded hover:bg-[#e6f8fe]',
+                    'bg-[#e6f8fe]' => request()->routeIs('measure.*')
+                ])>
+                    <details class="group cursor-pointer" @if(request()->routeIs('measure.*')) open @endif>
+                        <summary class="flex items-center space-x-2 text-gray-700">
+                            <img src="/src/public/images/policy-listicon.png" class="w-6 h-6" alt="">
+                            <span class="flex-1">施策一覧</span>
+                            <span class="ml-auto transition-transform group-open:rotate-90">▶</span>
+                        </summary>
+                        <ul class="pl-6 mt-4 space-y-1">
+                            <li><a href="{{ route('measure.create') }}" class="text-gray-600 hover:text-blue-600">実行タスク一覧</a></li>
+                            <li><a href="{{ route('measure.mentor') }}" class="text-gray-600 hover:text-blue-600">評価/改善未対応タスク一覧</a></li>
+                            <li><a href="{{ route('measure.deepdive') }}" class="text-gray-600 hover:text-blue-600">評価/改善済みタスク一覧</a></li>
+                        </ul>
+                    </details>
+                </li> --}}
+                </li>
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                    <details class="group cursor-pointer">
+                        <summary class="flex justify-between items-center text-gray-700">
+                            <div class="flex items-center space-x-2">
+                                <img src="images/survey-configiconicon.png" class="w-6 h-6" alt="">
                                 <span>アンケート設定</span>
                             </div>
                             <span class="transition-transform group-open:rotate-90">▶</span>
@@ -113,14 +146,14 @@
                             <li><a href="#" class="text-gray-600 hover:text-blue-600">アンケート一覧</a></li>
                         </ul>
                     </details>
-                </li> --}}
-                <li @class([
+                </li>
+                {{-- <li @class([
                     'p-4 rounded hover:bg-[#e6f8fe]',
                     'bg-[#e6f8fe]' => request()->routeIs('survey.*')
                 ])>
                     <details class="group cursor-pointer" @if(request()->routeIs('survey.*')) open @endif>
                         <summary class="flex items-center space-x-2 text-gray-700">
-                            <img src="./assets/img/アンケート設定icon.png" class="w-6 h-6" alt="">
+                            <img src="/src/public/images/survey-configiconicon.png" class="w-6 h-6" alt="">
                             <span class="flex-1">アンケート設定</span>
                             <span class="ml-auto transition-transform group-open:rotate-90">▶</span>
                         </summary>
@@ -129,12 +162,12 @@
                             <li><a href="{{ route('survey.index') }}" class="text-gray-600 hover:text-blue-600">アンケート一覧</a></li>
                         </ul>
                     </details>
-                </li>
-                {{-- <li class="hover:bg-[#e6f8fe] p-4 rounded">
+                </li> --}}
+                <li class="hover:bg-[#e6f8fe] p-4 rounded">
                     <details class="group cursor-pointer">
                         <summary class="flex justify-between items-center text-gray-700">
                             <div class="flex items-center space-x-2">
-                            <img src="./assets/img/設定icon.png" class="w-6 h-6" alt="">
+                            <img src="images/settingsicon.png" class="w-6 h-6" alt="">
                             <span>設定</span>
                             </div>
                             <span class="transition-transform group-open:rotate-90">▶</span>
@@ -143,14 +176,14 @@
                             <li><a href="#" class="text-gray-600 hover:text-blue-600">項目設定</a></li>
                         </ul>
                     </details>
-                </li> --}}
-                <li @class([
+                </li>
+                {{-- <li @class([
                     'p-4 rounded hover:bg-[#e6f8fe]',
                     'bg-[#e6f8fe]' => request()->routeIs('setting.*')
                 ])>
                     <details class="group cursor-pointer" @if(request()->routeIs('setting.*')) open @endif>
                         <summary class="flex items-center space-x-2 text-gray-700">
-                            <img src="./assets/img/設定icon.png" class="w-6 h-6" alt="">
+                            <img src="/src/public/images/settingsicon.png" class="w-6 h-6" alt="">
                             <span class="flex-1">設定</span>
                             <span class="ml-auto transition-transform group-open:rotate-90">▶</span>
                         </summary>
@@ -158,7 +191,7 @@
                             <li><a href="{{ route('setting.item') }}" class="text-gray-600 hover:text-blue-600">項目設定</a></li>
                         </ul>
                     </details>
-                </li>
+                </li> --}}
             </ul>
         </nav>
     </aside>
