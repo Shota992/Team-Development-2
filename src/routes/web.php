9b71b812a4dabe3e2cbd1fa33b4298857ea4d46c
hiroo-road <hirofumi7678@gmail.com>
2025-03-24 16:34:36 +0900

    add:フォーム機能の追加（実装完了）

--- src/routes/web.php
+++ src/routes/web.php
@@ -34,7 +34,7 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/departments', [DepartmentsController::class, 'index'])->name('dashboard');
-    Route::get('/measures', [MeasureController::class, 'index'])->name('dashboard');
+    Route::get('/measures', [MeasureController::class, 'index'])->name('measure.index');
 });
 
 Route::middleware('auth')->group(function () {
