1a2e1a3c8efce05af9dc639e8ec4129e541c49c6
hiroo-road <hirofumi7678@gmail.com>
2025-04-06 14:58:27 +0900

    add:サイドバーにて、現在表示箇所を青色に変更するようにする

--- src/resources/views/components/sidebar.blade.php
+++ src/resources/views/components/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="w-56 h-screen bg-white shadow-lg fixed overflow-y-auto overflow-x-hidden z-40">
-    <div class="py-4 border-b text-center">
-        <a href="{{ route('dashboard') }}" class="text-lg font-bold text-gray-700">get mild</a>
+    <div class="py-3 border-b text-center">
+        <a href="{{ route('dashboard') }}" class="text-lg font-bold text-gray-700"><img src="{{ asset('images/Kompasslogo.jpeg')}}" alt="Kompass"></a>
     </div>
 
     <div class="p-4 border-b bg-gray-100">
@@ -78,46 +78,46 @@
     <!-- ナビゲーションメニュー -->
     <nav>
         <ul>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
+            <li class="hover:bg-chart-gray p-4 rounded {{ request()->routeIs('dashboard') ? 'bg-[#e6f8fe]'  : '' }}">
                 <a href="{{ route('dashboard') }}" class="flex items-center space-x-2 text-gray-700">
                     <img src="{{ asset('images/dashboardicon.png') }}" class="w-6 h-6" alt="Dashboard">
                     <span>ダッシュボード</span>
                 </a>
             </li>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
+            <li class="hover:bg-chart-gray p-4 rounded {{ request()->routeIs('items.index') ? 'bg-[#e6f8fe]' : '' }}">
                 <a href="{{ route('items.index') }}" class="flex items-center space-x-2 text-gray-700">
                     <img src="{{ asset('images/item-detail icon.png') }}" class="w-6 h-6" alt="Item Details">
                     <span>項目別詳細</span>
                 </a>
             </li>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
+            <li class="hover:bg-chart-gray p-4 rounded {{ request()->routeIs('departments.index') ? 'bg-[#e6f8fe]' : '' }}">
                 <a href="{{ route('departments.index') }}" class="flex items-center space-x-2 text-gray-700">
                     <img src="{{ asset('images/dept-compareicon.png') }}" class="w-6 h-6" alt="Department Compare">
                     <span>部署別比較</span>
                 </a>
             </li>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
-                <details class="group cursor-pointer">
-                    <summary class="flex justify-between items-center text-gray-700">
+            <li class="hover:bg-chart-gray rounded {{ request()->routeIs('create-policy', 'chat.index', 'mindmap.index') ? 'bg-[#e6f8fe]' : '' }}">
+                <details class="group cursor-pointer" {{ request()->routeIs('create-policy', 'chat.index', 'mindmap.index') ? 'open' : '' }}>
+                    <summary class="flex justify-between items-center text-gray-700 p-4">
                         <div class="flex items-center space-x-2">
                             <img src="{{ asset('images/policy-createicon.png') }}" class="w-6 h-6" alt="Policy Create">
                             <span>施策立案</span>
                         </div>
                         <span class="transition-transform group-open:rotate-90">▶</span>
                     </summary>
-                    <ul class="pl-6 mt-4 space-y-1">
-                        <li><a href="{{ route('create-policy') }}" class="text-gray-600 hover:text-blue-600">施策作成</a></li>
-                        <li><a href="{{ route('chat.index') }}" class="text-gray-600 hover:text-blue-600">AIメンター</a></li>
-                        <li><a href="{{ route('mindmap.index') }}" class="text-gray-600 hover:text-blue-600">施策深掘り</a></li>
+                    <ul class="space-y-1 pb-2">
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('create-policy') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('create-policy') }}" class="block w-full text-gray-600">施策作成</a></li>
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('chat.index') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('chat.index') }}" class="block w-full text-gray-600">AIメンター</a></li>
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('mindmap.index') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('mindmap.index') }}" class="block w-full text-gray-600">施策深掘り</a></li>
                     </ul>
                 </details>
             </li>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
+            <li class="hover:bg-chart-gray rounded {{ request()->routeIs('measures.index', 'measure.no-evaluation' , 'measures.evaluation-list') ? 'bg-[#e6f8fe]' : '' }}">
                 @php
                     $totalTaskBadgeCount = ($executingTasksCount ?? 0) + ($pendingEvaluationMeasuresCount ?? 0);
                 @endphp
-                <details class="group cursor-pointer">
-                    <summary class="flex justify-between items-center text-gray-700">
+                <details class="group cursor-pointer" {{ request()->routeIs('measures.index', 'measure.no-evaluation' , 'measures.evaluation-list') ? 'open' : '' }}>
+                    <summary class="flex justify-between items-center text-gray-700 p-4">
                         <div class="flex items-center space-x-2">
                             <img src="{{ asset('images/policy-listicon.png') }}" class="w-6 h-6" alt="Policy List">
                             <span>施策一覧</span>
@@ -129,56 +129,61 @@
                         </div>
                         <span class="transition-transform group-open:rotate-90">▶</span>
                     </summary>
-                    <ul class="pl-6 mt-4 space-y-1">
-                        <li class="flex justify-between items-center">
-                            <a href="{{ route('measures.index') }}" class="text-gray-600 hover:text-blue-600">実行タスク一覧</a>
+                    <ul class="space-y-1 pb-2">
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('measures.index') ? 'bg-[#cff2fe]' : '' }} flex justify-between items-center">
+                            <a href="{{ route('measures.index') }}" class="block w-full text-gray-600">実行施策一覧
                             @if($executingTasksCount > 0)
                                 <span class="bg-blue-500 text-white text-xs font-bold rounded-full px-2 py-0.5">
                                     {{ $executingTasksCount }}
                                 </span>
                             @endif
+                            </a>
                         </li>
-                        <li class="flex justify-between items-center">
-                            <a href="{{ route('measure.no-evaluation') }}" class="text-gray-600 hover:text-blue-600">評価/改善未対応タスク一覧</a>
+                        <li class="pl-6 hover:bg-chart-do-gray  {{ request()->routeIs('measure.no-evaluation') ? 'bg-[#cff2fe]' : '' }} flex justify-between items-center">
+                            <a href="{{ route('measure.no-evaluation') }}" class="block w-full text-gray-600">評価/改善未対応施策一覧
                             @if($pendingEvaluationMeasuresCount > 0)
                                 <span class="bg-blue-500 text-white text-xs font-bold rounded-full px-2 py-0.5">
                                     {{ $pendingEvaluationMeasuresCount }}
                                 </span>
                             @endif
+                            </a>
                         </li>
-                        <li class="flex justify-between items-center">
-                            <a href="{{ route('measures.evaluation-list') }}" class="text-gray-600 hover:text-blue-600">評価/改善済みタスク一覧</a>
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('measures.evaluation-list') ? 'bg-[#cff2fe]' : '' }} flex justify-between items-center">
+                            <a href="{{ route('measures.evaluation-list') }}" class="block w-full text-gray-600">評価/改善済み施策一覧</a>
                         </li>
                     </ul>
                 </details>
             </li>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
-                <details class="group cursor-pointer">
-                    <summary class="flex justify-between items-center text-gray-700">
+            <li class="hover:bg-chart-gray rounded {{ request()->routeIs('survey.create', 'survey.list') ? 'bg-[#e6f8fe]' : '' }}">
+                <details class="group cursor-pointer" {{ request()->routeIs('survey.create', 'survey.list') ? 'open' : '' }}>
+                    <summary class="flex justify-between items-center text-gray-700 p-4">
                         <div class="flex items-center space-x-2">
                             <img src="{{ asset('images/survey-configiconicon.png') }}" class="w-6 h-6" alt="Survey Config">
                             <span>アンケート設定</span>
                         </div>
                         <span class="transition-transform group-open:rotate-90">▶</span>
                     </summary>
-                    <ul class="pl-6 mt-4 space-y-1">
-                        <li><a href="{{ route('survey.create') }}" class="text-gray-600 hover:text-blue-600">アンケート作成</a></li>
-                        <li><a href="{{ route('survey.list') }}" class="text-gray-600 hover:text-blue-600">アンケート一覧</a></li>
+                    <ul class="space-y-1 pb-2">
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('survey.create') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('survey.create') }}" class="block w-full text-gray-600">アンケート作成</a></li>
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('survey.list') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('survey.list') }}" class="block w-full text-gray-600">アンケート一覧</a></li>
                     </ul>
                 </details>
             </li>
-            <li class="hover:bg-[#e6f8fe] p-4 rounded">
-                <details class="group cursor-pointer">
-                    <summary class="flex justify-between items-center text-gray-700">
+            <li class="hover:bg-chart-gray rounded {{ request()->routeIs('survey_questions.index', 'setting.employee-list' , 'employee.create') ? 'bg-[#e6f8fe]' : '' }}">
+                <details class="group cursor-pointer" {{ request()->routeIs('survey_questions.index', 'setting.employee-list' , 'employee.create') ? 'open' : '' }}>
+                    <summary class="flex justify-between items-center text-gray-700 p-4">
                         <div class="flex items-center space-x-2">
                             <img src="{{ asset('images/settingsicon.png') }}" class="w-6 h-6" alt="Settings">
                             <span>設定</span>
                         </div>
                         <span class="transition-transform group-open:rotate-90">▶</span>
                     </summary>
-                    <ul class="pl-6 mt-4 space-y-1">
-                        <li><a href="{{ route('survey_questions.index') }}" class="text-gray-600 hover:text-blue-600">項目設定</a></li>
+                    <ul class="space-y-1 pb-2">
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('survey_questions.index') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('survey_questions.index') }}" class="block w-full text-gray-600">項目設定</a></li>
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('setting.employee-list') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('setting.employee-list') }}" class="block w-full text-gray-600">従業員一覧</a></li>
+                        <li class="pl-6 hover:bg-chart-do-gray {{ request()->routeIs('employee.create') ? 'bg-[#cff2fe]' : '' }}"><a href="{{ route('employee.create') }}" class="block w-full text-gray-600">従業員登録</a></li>
                     </ul>
+                    <ul class="pl-6 mt-4 space-y-1">
                 </details>
             </li>
         </ul>
